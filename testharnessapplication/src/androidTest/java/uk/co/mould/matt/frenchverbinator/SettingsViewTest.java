5fa17ea5d2adb6a267305da355154e4e1e6a5b49
Matt Mould <matthew.mould@bbc.co.uk>
2015-07-03 18:22:34 +0100

    travis

--- testharnessapplication/src/androidTest/java/uk/co/mould/matt/frenchverbinator/SettingsViewTest.java
+++ testharnessapplication/src/androidTest/java/uk/co/mould/matt/frenchverbinator/SettingsViewTest.java
@@ -76,7 +76,6 @@ public class SettingsViewTest extends ActivityInstrumentationTestCase2<TestActiv
     }
 
     public void testThatPresenterIsToldWhenTenseIsUnchecked() {
-
         tellViewToAddTenseOptions(presentConditional, imperfectIndicative);
         tellViewToSelectRequiredTenseOptions(presentConditional);
         tellViewToSelectRequiredTenseOptions(imperfectIndicative);
