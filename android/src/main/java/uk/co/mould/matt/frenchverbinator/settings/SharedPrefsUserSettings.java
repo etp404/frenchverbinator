7355975b8ae414b263b83dc4a1352610159c87d5
Matt Mould <matthew.mould@bbc.co.uk>
2015-09-17 16:28:32 +0100

    Defaults to just present indicative.

--- android/src/main/java/uk/co/mould/matt/frenchverbinator/settings/SharedPrefsUserSettings.java
+++ android/src/main/java/uk/co/mould/matt/frenchverbinator/settings/SharedPrefsUserSettings.java
@@ -9,13 +9,14 @@ import java.util.Set;
 
 import uk.co.mould.matt.data.SupportedMoodsAndTenses;
 import uk.co.mould.matt.data.tenses.MoodAndTense;
+import uk.co.mould.matt.data.tenses.PresentIndicative;
 
 //To much complexity in here: need to split out some stuff.
 public class SharedPrefsUserSettings implements StoredUserSettings {
     public static final String SETTINGS = "verbinator_prefs";
     private static final String INCLUDED_TENSES = "included_tenses";
     private static final Set<String> DEFAULT_TENSES = new HashSet<String>(){{
-        addAll(convertToStringSet(SupportedMoodsAndTenses.ALL));
+        add(new PresentIndicative().toString());
     }};
 
     private SharedPreferences sharedPreferences;
