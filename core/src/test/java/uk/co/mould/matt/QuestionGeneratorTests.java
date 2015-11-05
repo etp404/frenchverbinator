8fb600c1e453d71ed0e69fb33bceb68daf519374
Matt Mould <matthew.mould@bbc.co.uk>
2015-11-05 13:02:36 +0000

    Moving tests around question generation into core.

--- core/src/test/java/uk/co/mould/matt/QuestionGeneratorTests.java
+++ core/src/test/java/uk/co/mould/matt/QuestionGeneratorTests.java
@@ -3,17 +3,23 @@ package uk.co.mould.matt;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import uk.co.mould.matt.data.InfinitiveVerb;
 import uk.co.mould.matt.data.Persons;
 import uk.co.mould.matt.data.tenses.MoodAndTense;
+import uk.co.mould.matt.data.tenses.PresentIndicative;
 import uk.co.mould.matt.questions.Callback;
 import uk.co.mould.matt.questions.Question;
 import uk.co.mould.matt.questions.QuestionGenerator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class QuestionGeneratorTests {
+    private final Persons.Person person = Persons.FIRST_PERSON_PLURAL;
+    private final InfinitiveVerb verb = new InfinitiveVerb("regarder", "to watch", null);
+    private MoodAndTense verbMoodAndTense = new PresentIndicative();
 
     @Test
     public void returnsNoTensesSelectedIfNoTensesAreSelected() {
@@ -29,12 +35,28 @@ public class QuestionGeneratorTests {
         assertTrue(callback.noTensesSelected);
     }
 
+    @Test
+    public void returnsQuestionFromList() {
+        QuestionGenerator questionGenerator = new QuestionGenerator(new FakeRandomQuestionGenerator(),
+                Collections.singletonList(verb),
+                Collections.singletonList(person),
+                Collections.singletonList(verbMoodAndTense));
+
+        CapturingCallback callback = new CapturingCallback();
+        questionGenerator.getQuestion(callback);
+
+        Question expectedQuestion = new Question(person, verb, verbMoodAndTense);
+
+        assertEquals(expectedQuestion, callback.question);
+    }
+
     private static class CapturingCallback implements Callback {
         private boolean noTensesSelected = false;
+        private Question question;
 
         @Override
         public void questionProvided(Question question) {
-
+            this.question = question;
         }
 
         @Override
