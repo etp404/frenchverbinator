9655377fb3f8b010e951ab834550f2bd5406c718
Matt <matthewsimonmould@gmail.com>
2015-11-11 19:38:54 +0000

    Random questions now repeated 75 percent of the time.

--- app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionPresenterFactory.java
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionPresenterFactory.java
@@ -5,13 +5,16 @@ import android.content.Context;
 import org.xml.sax.InputSource;
 
 import uk.co.mould.matt.FailedQuestionStore;
+import uk.co.mould.matt.ShouldUseFailedQuestion;
 import uk.co.mould.matt.conjugators.Conjugator;
 import uk.co.mould.matt.data.SupportedPersons;
+import uk.co.mould.matt.frenchverbinator.failedquestions.AndroidFailedQuestionStore;
 import uk.co.mould.matt.frenchverbinator.settings.SharedPrefsUserSettings;
 import uk.co.mould.matt.marking.AnswerChecker;
 import uk.co.mould.matt.parser.ConjugationParser;
 import uk.co.mould.matt.parser.VerbListParser;
 import uk.co.mould.matt.parser.VerbTemplateParser;
+import uk.co.mould.matt.questions.RandomNumberGenerator;
 import uk.co.mould.matt.questions.RandomQuestionGenerator;
 import uk.co.mould.matt.questions.SystemRandomNumberGenerator;
 
@@ -24,15 +27,27 @@ class QuestionPresenterFactory {
         ConjugationParser conjugationParser = new ConjugationParser(new InputSource(context.getResources().openRawResource(R.raw.conjugation_fr)));
         VerbListParser verbListParser = new VerbListParser(new InputSource(context.getResources().openRawResource(R.raw.verb_list)));
         Conjugator conjugator = new Conjugator(verbTemplateParser, conjugationParser);
+
+        FailedQuestionStore failedQuestionStore = new AndroidFailedQuestionStore(context);
+        ShouldUseFailedQuestion returnTrue75PercentOfTime = new ShouldUseFailedQuestion() {
+            RandomNumberGenerator randomNumberGenerator = new SystemRandomNumberGenerator();
+
+            @Override
+            public boolean invoke() {
+                return randomNumberGenerator.randomNumber(0, 100) > 75;
+            }
+        };
         RandomQuestionGenerator randomQuestionGenerator = new RandomQuestionGenerator(
                 new SystemRandomNumberGenerator(),
                 verbListParser.getVerbs(),
                 SupportedPersons.ALL,
-                storedUserSettings.includedTenses());
+                storedUserSettings.includedTenses(),
+                failedQuestionStore,
+                returnTrue75PercentOfTime);
 
         new QuestionPresenter(
                 questionView,
                 randomQuestionGenerator,
-                new AnswerChecker(conjugator, FailedQuestionStore.NULL));
+                new AnswerChecker(conjugator, failedQuestionStore));
     }
 }
