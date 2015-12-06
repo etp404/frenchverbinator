55f9fe94342fa7d802a78e01d53ae4a7f8d7d4e1
Matt <matthewsimonmould@gmail.com>
2015-12-06 11:31:51 +0000

    Switched from actionbar to toolbar.

--- app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionActivity.java
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionActivity.java
@@ -3,6 +3,7 @@ package uk.co.mould.matt.frenchverbinator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class QuestionActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.question_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         QuestionPresenterFactory.create(getApplicationContext(), ((AndroidQuestionView) findViewById(R.id.android_question_view)));
     }
 
--- app/src/main/java/uk/co/mould/matt/frenchverbinator/SettingsActivity.java
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/SettingsActivity.java
@@ -1,7 +1,11 @@
 package uk.co.mould.matt.frenchverbinator;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ViewGroup;
 
 import uk.co.mould.matt.frenchverbinator.settings.ui.AndroidSettingsView;
@@ -16,6 +20,15 @@ public final class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity_layout);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Drawable upArrow = ContextCompat.getDrawable(getApplicationContext(), R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        upArrow.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.icons), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         AndroidSettingsView settingsView = (AndroidSettingsView) findViewById(R.id.android_settings_view);
 
