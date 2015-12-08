e679dfc87f4d02a73a84cd0b96c75f5ccdf909db
Matt <matthewsimonmould@gmail.com>
2015-12-08 21:34:36 +0000

    Back button added to feedback page

--- app/src/main/java/uk/co/mould/matt/frenchverbinator/FeedbackActivity.java
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/FeedbackActivity.java
@@ -1,6 +1,9 @@
 package uk.co.mould.matt.frenchverbinator;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -17,6 +20,11 @@ public class FeedbackActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final Drawable upArrow = ContextCompat.getDrawable(getApplicationContext(), R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        upArrow.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.icons), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         AndroidFeedbackView feedbackView = (AndroidFeedbackView) findViewById(R.id.android_feedback_form);
         new FeedbackFormPresenter(feedbackView, new FeedbackEmailLauncher(getApplicationContext()));
