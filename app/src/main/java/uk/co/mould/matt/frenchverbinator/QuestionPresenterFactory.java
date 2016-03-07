99c2462fe4ad6124440782c2ca049d90183cf6e0
Matt Mould <matthew.mould@bbc.co.uk>
2016-03-07 12:29:22 +0000

    The question generation classes are now created in the application.

--- app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionPresenterFactory.java
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/QuestionPresenterFactory.java
@@ -2,52 +2,14 @@ package uk.co.mould.matt.frenchverbinator;
 
 import android.content.Context;
 
-import org.xml.sax.InputSource;
-
-import uk.co.mould.matt.FailedQuestionStore;
-import uk.co.mould.matt.ShouldUseFailedQuestion;
-import uk.co.mould.matt.conjugators.Conjugator;
-import uk.co.mould.matt.data.SupportedPersons;
-import uk.co.mould.matt.frenchverbinator.failedquestions.AndroidFailedQuestionStore;
-import uk.co.mould.matt.frenchverbinator.settings.SharedPrefsUserSettings;
-import uk.co.mould.matt.marking.AnswerChecker;
-import uk.co.mould.matt.parser.ConjugationParser;
-import uk.co.mould.matt.parser.VerbListParser;
-import uk.co.mould.matt.parser.VerbTemplateParser;
-import uk.co.mould.matt.questions.RandomNumberGenerator;
-import uk.co.mould.matt.questions.RandomQuestionGenerator;
-import uk.co.mould.matt.questions.SystemRandomNumberGenerator;
-
 class QuestionPresenterFactory {
 
     public static void create(Context context, AndroidQuestionView questionView) {
-        SharedPrefsUserSettings storedUserSettings = new SharedPrefsUserSettings(context.getSharedPreferences(SharedPrefsUserSettings.SETTINGS, 0));
-        VerbTemplateParser verbTemplateParser = new VerbTemplateParser(new InputSource(context.getResources().openRawResource(
-                R.raw.verbs_fr)));
-        ConjugationParser conjugationParser = new ConjugationParser(new InputSource(context.getResources().openRawResource(R.raw.conjugation_fr)));
-        VerbListParser verbListParser = new VerbListParser(new InputSource(context.getResources().openRawResource(R.raw.verb_list)));
-        Conjugator conjugator = new Conjugator(verbTemplateParser, conjugationParser);
-
-        FailedQuestionStore failedQuestionStore = new AndroidFailedQuestionStore(context);
-        ShouldUseFailedQuestion shouldUseFailedQuestion25PercentOfTime = new ShouldUseFailedQuestion() {
-            RandomNumberGenerator randomNumberGenerator = new SystemRandomNumberGenerator();
-
-            @Override
-            public boolean invoke() {
-                return randomNumberGenerator.randomNumber(0, 100) > 75;
-            }
-        };
-        RandomQuestionGenerator randomQuestionGenerator = new RandomQuestionGenerator(
-                new SystemRandomNumberGenerator(),
-                verbListParser.getVerbs(),
-                SupportedPersons.ALL,
-                storedUserSettings.includedTenses(),
-                failedQuestionStore,
-                shouldUseFailedQuestion25PercentOfTime);
+        VerbinatorApplication verbinatorApplication = (VerbinatorApplication)context.getApplicationContext();
 
         new QuestionPresenter(
                 questionView,
-                randomQuestionGenerator,
-                new AnswerChecker(conjugator, failedQuestionStore));
+                verbinatorApplication.getRandomQuestionGenerator(),
+                verbinatorApplication.getAnswerChecking());
     }
 }
---
+++ app/src/main/java/uk/co/mould/matt/frenchverbinator/VerbinatorApplication.java
@@ -0,0 +1,71 @@
+package uk.co.mould.matt.frenchverbinator;
+
+import android.app.Application;
+
+import org.xml.sax.InputSource;
+
+import uk.co.mould.matt.FailedQuestionStore;
+import uk.co.mould.matt.ShouldUseFailedQuestion;
+import uk.co.mould.matt.conjugators.Conjugator;
+import uk.co.mould.matt.data.SupportedPersons;
+import uk.co.mould.matt.frenchverbinator.failedquestions.AndroidFailedQuestionStore;
+import uk.co.mould.matt.frenchverbinator.settings.SharedPrefsUserSettings;
+import uk.co.mould.matt.marking.AnswerChecker;
+import uk.co.mould.matt.marking.AnswerChecking;
+import uk.co.mould.matt.parser.ConjugationParser;
+import uk.co.mould.matt.parser.VerbListParser;
+import uk.co.mould.matt.parser.VerbTemplateParser;
+import uk.co.mould.matt.questions.QuestionGenerator;
+import uk.co.mould.matt.questions.RandomNumberGenerator;
+import uk.co.mould.matt.questions.RandomQuestionGenerator;
+import uk.co.mould.matt.questions.SystemRandomNumberGenerator;
+
+/**
+ * Copyright © 2016 Media Applications Technologies. All rights reserved.
+ */
+public class VerbinatorApplication  extends Application {
+
+    private AnswerChecker answerChecking;
+    private RandomQuestionGenerator randomQuestionGenerator;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        VerbTemplateParser verbTemplateParser = new VerbTemplateParser(new InputSource(getResources().openRawResource(
+                R.raw.verbs_fr)));
+        ConjugationParser conjugationParser = new ConjugationParser(new InputSource(getResources().openRawResource(R.raw.conjugation_fr)));
+        Conjugator conjugator = new Conjugator(verbTemplateParser, conjugationParser);
+
+        FailedQuestionStore failedQuestionStore = new AndroidFailedQuestionStore(this);
+
+        answerChecking = new AnswerChecker(conjugator, failedQuestionStore);
+        SharedPrefsUserSettings storedUserSettings = new SharedPrefsUserSettings(getSharedPreferences(SharedPrefsUserSettings.SETTINGS, 0));
+        VerbListParser verbListParser = new VerbListParser(new InputSource(getResources().openRawResource(R.raw.verb_list)));
+
+
+        ShouldUseFailedQuestion shouldUseFailedQuestion25PercentOfTime = new ShouldUseFailedQuestion() {
+            RandomNumberGenerator randomNumberGenerator = new SystemRandomNumberGenerator();
+
+            @Override
+            public boolean invoke() {
+                return randomNumberGenerator.randomNumber(0, 100) > 75;
+            }
+        };
+
+        randomQuestionGenerator = new RandomQuestionGenerator(
+                new SystemRandomNumberGenerator(),
+                verbListParser.getVerbs(),
+                SupportedPersons.ALL,
+                storedUserSettings.includedTenses(),
+                failedQuestionStore,
+                shouldUseFailedQuestion25PercentOfTime);
+    }
+
+    public QuestionGenerator getRandomQuestionGenerator() {
+        return randomQuestionGenerator;
+    }
+
+    public AnswerChecking getAnswerChecking() {
+        return answerChecking;
+    }
+}
